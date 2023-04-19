7de8b0f6bd64599c43f53a4b551a694f75f0fe13
Bo Wen <bowenwen@users.noreply.github.com>
2023-04-19 05:39:15 +0000

    customize prompt for alpaca

--- langchain/agents/mrkl/prompt.py
+++ langchain/agents/mrkl/prompt.py
@@ -1,16 +1,42 @@
 # flake8: noqa
-PREFIX = """Answer the following questions as best you can. You have access to the following tools:"""
+# version modified for alpaca
+PREFIX = """Below is an instruction that describes a task. 
+
+### Instruction: 
+
+Answer the following questions as best you can. You have access to the following tools:"""
 FORMAT_INSTRUCTIONS = """Use the following format:
 
 Question: the input question you must answer
-Thought: you should always think about what to do
+Thought: you should always think about what to do next based on what you already know
 Action: the action to take, should be one of [{tool_names}]
-Action Input: the input to the action
+Action Input: the input to the action to obtain additional information to the question
 Observation: the result of the action
 ... (this Thought/Action/Action Input/Observation can repeat N times)
 Thought: I now know the final answer
-Final Answer: the final answer to the original input question"""
-SUFFIX = """Begin!
+Final Answer: end the conversation by summarizing your findings"""
+SUFFIX = """
+### Input:
 
 Question: {input}
+
+### Response:
+
 Thought:{agent_scratchpad}"""
+
+# original langchain version
+# PREFIX = """Answer the following questions as best you can. You have access to the following tools:"""
+# FORMAT_INSTRUCTIONS = """Use the following format:
+
+# Question: the input question you must answer
+# Thought: you should always think about what to do
+# Action: the action to take, should be one of [{tool_names}]
+# Action Input: the input to the action
+# Observation: the result of the action
+# ... (this Thought/Action/Action Input/Observation can repeat N times)
+# Thought: I now know the final answer
+# Final Answer: the final answer to the original input question"""
+# SUFFIX = """Begin!
+
+# Question: {input}
+# Thought:{agent_scratchpad}"""
